e8f25cd2921d37d96ee1d782c8ba9bc68c0dc0f7
Theophilus Siameh <10055091+donwany@users.noreply.github.com>
2023-01-16 17:55:00 -0600

    Delete notification directory

--- notification/SECRETS.py
+++
@@ -1,3 +0,0 @@
-api_key = '0cd65e7c141545cb1d64830930ae860e'
-api_secret = '23d36a2f17a80fa26db09f786b8a714d'
-bit_coin_keys = 'c6399795-8d30-4de7-88a2-752f461986d4'
--- notification/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build --tag worldbosskafka/email-svc:v1.0.8 . -f Dockerfile
-docker push worldbosskafka/email-svc:v1.0.8
